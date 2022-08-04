c602eac66491e4054fbd8ed66eae415123fbbd2a
P <p@enigmalabs.io>
2022-08-04 14:32:25 -0400

    dev: use tippecanoe clustering example args

--- scripts/03-geojson-to-mbtiles.sh
+++ scripts/03-geojson-to-mbtiles.sh
@@ -6,4 +6,8 @@ cd $(dirname "$0")/.. || exit 1
 
 # Generate MBTile tileset from GeoJSON
 echo "Generating MBTile tileset from GeoJSON..."
-tippecanoe -f -zg -o data/out.mbtiles --drop-densest-as-needed data/incidents.geojson
+
+# -zg: Automatically choose a maxzoom that should be sufficient to clearly distinguish the features and the detail within each feature
+# -r1: Do not automatically drop a fraction of points at low zoom levels, since clustering will be used instead
+# --cluster-distance=10: Cluster together features that are closer than about 10 pixels from each other
+tippecanoe -f -zg -r1 --cluster-distance 10 -o data/out.mbtiles data/incidents.geojson
