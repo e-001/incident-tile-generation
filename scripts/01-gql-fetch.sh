87163c7d3b727b47d421ebbb0a5c898356efe4d7
P <p@enigmalabs.io>
2022-08-05 00:00:05 -0400

    feat: generate unclustered set with year as prop

--- scripts/01-gql-fetch.sh
+++ scripts/01-gql-fetch.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+cd $(dirname "$0")/.. || exit 1
+
 # Download surveys using graphqurl
 echo "Downloading incident_survey feature objects... this may take a while..."
 graphqurl https://enigma.hasura.app/v1/graphql \
     -H "x-hasura-admin-secret: ${GRAPHQL_TOKEN}" \
-    -q "query FeatureCollection(\$where: incident_survey_bool_exp, \$limit: Int) @cached { incident_survey(where: \$where, limit: \$limit) { feature } }" \
+    -q "query FeatureCollection(\$where: incident_survey_bool_exp, \$limit: Int) @cached { incident_survey(where: \$where, limit: \$limit) { feature incident { year } } }" \
     > data/surveys.json
--- src/gql-to-geojson.ts
+++ src/gql-to-geojson.ts
@@ -1,7 +1,13 @@
 import { writeFileSync } from "fs"
 
 const raw = require('../data/surveys.json');
-const features = raw.data.incident_survey.map((s: any) => { return s.feature });
+const features = raw.data.incident_survey
+  .filter((s: any) => s.feature !== null)
+  .map((s: any) => {
+  var geojson = s.feature
+  geojson.properties.year = s.incident.year
+  return geojson
+})
 const collection = {
   type: 'FeatureCollection',
   features: features
