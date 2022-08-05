314ecef897b199b376d0b0fb93f7eb0a548652ac
P <p@enigmalabs.io>
2022-08-04 21:01:00 -0400

    feat: include cluster incident_id at high zoom

--- scripts/03-geojson-to-mbtiles.sh
+++ scripts/03-geojson-to-mbtiles.sh
@@ -9,5 +9,9 @@ echo "Generating MBTile tileset from GeoJSON..."
 
 # -zg: Automatically choose a maxzoom that should be sufficient to clearly distinguish the features and the detail within each feature
 # -r1: Do not automatically drop a fraction of points at low zoom levels, since clustering will be used instead
+# --accumulate-attribute store concat'd inciden_ids for cluster expansion
+# -j { filter } filter out the long list of incident_ids at zooms higher than 9
 # --cluster-distance=10: Cluster together features that are closer than about 10 pixels from each other
-tippecanoe -f -zg -r1 --cluster-distance 20 -o data/out.mbtiles data/incidents.geojson
+tippecanoe -f -zg -r1 --cluster-distance 30 --accumulate-attribute=incident_id:comma \
+    -j '{ "*": [ "attribute-filter", "incident_id", [ ">=", "$zoom", 9 ] ] }' \
+    -o data/out.mbtiles data/incidents.geojson
