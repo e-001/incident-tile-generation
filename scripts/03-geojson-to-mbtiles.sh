753d7550bc4286b26edac4aa2c70f36ebeff6087
P <p@enigmalabs.io>
2022-08-04 23:19:34 -0400

    dev: persist ids in clusters at high zooms

--- scripts/03-geojson-to-mbtiles.sh
+++ scripts/03-geojson-to-mbtiles.sh
@@ -7,11 +7,10 @@ cd $(dirname "$0")/.. || exit 1
 # Generate MBTile tileset from GeoJSON
 echo "Generating MBTile tileset from GeoJSON..."
 
-# -zg: Automatically choose a maxzoom that should be sufficient to clearly distinguish the features and the detail within each feature
-# -r1: Do not automatically drop a fraction of points at low zoom levels, since clustering will be used instead
-# --accumulate-attribute store concat'd inciden_ids for cluster expansion
-# -j { filter } filter out the long list of incident_ids at zooms higher than 9
-# --cluster-distance=10: Cluster together features that are closer than about 10 pixels from each other
-tippecanoe -f -zg -r1 --cluster-distance 30 --accumulate-attribute=incident_id:comma \
-    -j '{ "*": [ "attribute-filter", "incident_id", [ ">=", "$zoom", 9 ] ] }' \
-    -o data/out.mbtiles data/incidents.geojson
+tippecanoe -f -r1 --cluster-distance 30 -Z0 -z8 --coalesce \
+    -o data/lowzoom.mbtiles data/incidents.geojson
+# Keep incident_id intact above z8 for cluster expansion with accumulate-attribute store concat'd incident_ids
+tippecanoe -f -r1 --cluster-distance 30 -Z8 -z15 --coalesce --accumulate-attribute=incident_id:comma \
+    -o data/highzoom.mbtiles data/incidents.geojson
+# Join them for upload
+tile-join -f -o data/incidents.mbtiles data/lowzoom.mbtiles data/highzoom.mbtiles
